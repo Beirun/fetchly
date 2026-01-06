190f8c57faff0c6ec63e2f6672e01d7f75e3da3e
Beirun <orillobernardjay@gmail.com>
2026-01-06 10:01:16 +0800

    Updated trail middleware

--- server/middleware/remove-trail.ts
+++ server/middleware/remove-trail.ts
@@ -1,19 +1,19 @@
-export default defineEventHandler(async (e) => {
+export default defineEventHandler(async (event) => {
   const removeTrail = (url: string) => {
     const i = url.indexOf('&list')
     return i === -1 ? url : url.substring(0, i)
   }
 
-  const q = getQuery(e)
+  const q = getQuery(event)
   if (typeof q.url === 'string') {
     q.url = removeTrail(q.url)
   }
 
-  if (e.method === 'POST') {
-    const b = await readBody<any>(e)
+  if (event.method === 'POST') {
+    const b = await readBody<any>(event)
     if (b?.url && typeof b.url === 'string') {
       b.url = removeTrail(b.url)
-      e.context.body = b
+      event.context.body = b
     }
   }
 })
